f289ed73fb7a4740a971dfe8ec944c0c31f9a03b
korbut.darya <korbut.darya@securitylab.by>
2025-10-11 00:31:39 +0300

    feat: add solutuibs to 4-7 taska

--- src/homework_4/HomeWorkFour.java
+++ src/homework_4/HomeWorkFour.java
@@ -4,25 +4,53 @@ import java.util.Scanner;
 
 public class HomeWorkFour {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         System.out.print("Введите количество элементов массива: ");
 
-        int arrLength = input.nextInt();
+        int arrLength = inputIntPositiveNumber(false);
 
-        int[] arr = generateArrItems(arrLength, 1, 100);
+        int[] arr = generateArrItems(arrLength, 0, 9);
+//        int[] arr = handleInputArrItems(arrLength);
 
         // Task 1
+        System.out.println("TASK 1 ==========");
         printArr(arr);
 
         // Task 2
+        System.out.println("TASK 2 ==========");
         int[] maxMin = getMaxMinNumbers(arr);
         System.out.printf("Min -> %d\n", maxMin[0]);
         System.out.printf("Max -> %d\n", maxMin[1]);
 
         // Task 3
+        System.out.println("TASK 3 ==========");
         int[] maxMinIdx = getMaxMinIdx(arr);
         System.out.printf("Min idx -> %d\n", maxMinIdx[0]);
         System.out.printf("Max idx -> %d\n", maxMinIdx[1]);
+
+        // Task 4
+        System.out.println("TASK 4 ==========");
+        int countZeroElements = getCountZeroElements(arr);
+        System.out.printf("Количество 0-ых элементов в массиве -> %d\n", countZeroElements);
+
+        // Task 5
+        System.out.println("TASK 5 ==========");
+        int[] swappedArr = swapElements(arr);
+        System.out.println("Swapped массив:");
+        printArr(swappedArr);
+        
+        // Task 6
+        boolean isIncreasing = isIncreasingArr(arr);
+        
+        if (isIncreasing) {
+            System.out.println("Возрастающая последовательность");
+        } else {
+            System.out.println("Не возрастающая последовательность");
+        }
+
+        // Task *
+        System.out.println("TASK * ==========");
+        int[] increaseArr = taskWithStar();
+        printArr(increaseArr);
     }
 
     // Task 1
@@ -49,6 +77,7 @@ public class HomeWorkFour {
 
         return new int[]{min, max};
     }
+
     // Task 3
     // Найти индексы минимального и максимального элементов и вывести в консоль
     public static int[] getMaxMinIdx(int[] arr) {
@@ -66,6 +95,93 @@ public class HomeWorkFour {
         return new int[]{minIdx, maxIdx};
     }
 
+    // Task 4
+    // Найти и вывести количество нулевых элементов. Если нулевых элементов нет - вывести
+    // сообщение, что их нет
+    public static int getCountZeroElements(int[] arr) {
+        int counter = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == 0) counter++;
+        }
+
+        return counter;
+    }
+
+    //    Пройти по массиву и поменять местами элементы первый и последний, второй и
+    //    предпоследний и т.д.
+    public static int[] swapElements(int[] arr) {
+        int[] localArr = copyArr(arr);
+        int variable;
+
+        for (int i = 0; i < arr.length / 2; i++) {
+            int endIndex = arr.length - 1 - i;
+            variable = localArr[i];
+            localArr[i] = localArr[endIndex];
+            localArr[endIndex] = variable;
+        }
+
+        return localArr;
+    }
+
+    //    Проверить, является ли массив возрастающей последовательностью (каждое следующее
+    //  число больше предыдущего).
+    public static boolean isIncreasingArr(int[] arr) {
+        boolean isIncrease = true;
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0) continue;
+            if (arr[i] < arr[i - 1]) {
+                isIncrease = false;
+                break;
+            }
+            
+        }
+        
+        return isIncrease;
+    }
+
+    //    Имеется массив из неотрицательных чисел(любой). Представьте что массив
+    //    представляет целое число (Например массив {1,2,3} -> 123, {9,9,9} -> 999). Задача
+    //    добавить единицу к этому “числу” и на выходе получить исправленный массив. Массив не
+    //    содержит нуля в начале, кроме самого числа 0.
+    public static int[] taskWithStar() {
+        System.out.print("Введите количество элементов массива: ");
+
+        int arrLength = inputIntPositiveNumber(false);
+        int[] arr = new int[arrLength];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("Введите %d-й элемент массива: ", i + 1);
+            arr[i] = inputIntPositiveNumber(arr.length == 1);
+        }
+
+        boolean isCrowded = false;
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int value = arr[i] + 1;
+
+            if (value < 10) {
+                arr[i] = value;
+                break;
+            }
+            arr[i] = 0;
+
+            if (i == 0) isCrowded = true;
+        }
+
+        if (!isCrowded) return arr;
+
+        int[] result = new int[arr.length + 1];
+        result[0] = 1;
+
+        for (int i = 1; i < result.length; i++) {
+            result[i] = arr[i - 1];
+        }
+
+        return  result;
+    }
+
     public static int[] handleInputArrItems(int length) {
         if (length < 0) {
             System.out.println("Вы ввели неккоректное число");
@@ -75,9 +191,8 @@ public class HomeWorkFour {
         int[] arr = new int[length];
 
         for (int i = 0; i < length; i++) {
-            Scanner inputItem = new Scanner(System.in);
             System.out.printf("Введите %d-й элемент массива: ", i + 1);
-            arr[i] = inputItem.nextInt();
+            arr[i] = inputIntPositiveNumber(true);
         }
 
         return arr;
@@ -104,4 +219,38 @@ public class HomeWorkFour {
         return (int) (Math.random() * (range + 1)) + min;
     }
 
+    public static int inputIntPositiveNumber(boolean isCanBeZero) {
+        System.out.println("Введите число: ");
+        Scanner input = new Scanner(System.in);
+        boolean isValidNumber = false;
+        int number = 0;
+
+        while (!isValidNumber) {
+            if (!input.hasNextInt()) {
+                System.out.println("Введеннное значение должно быть целым числом");
+                input.next();
+                continue;
+            }
+
+            number = input.nextInt();
+
+            if ((number > 0 && !isCanBeZero) || (isCanBeZero && number >= 0)) {
+                isValidNumber = true;
+            } else  {
+                System.out.println("Число должно быть положительным");
+            }
+        }
+
+        return number;
+    }
+
+    public static int[] copyArr(int[] arr) {
+        int[] localArr = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            localArr[i] = arr[i];
+        }
+
+        return localArr;
+    }
 }
