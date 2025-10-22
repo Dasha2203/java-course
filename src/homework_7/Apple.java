a1d62e8687f6eaab33c9abce5531f34f9ec12c05
Dasha2203 <kitkatlolipop@mail.ru>
2025-10-22 19:32:17 +0300

    feat: add apple class

---
+++ src/homework_7/Apple.java
@@ -0,0 +1,13 @@
+package homework_7;
+
+public class Apple {
+    private String color = "green";
+
+    public Apple(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+}
