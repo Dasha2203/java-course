92fde169df50eb1bfe14af3b962000fa12f6d358
Dasha2203 <kitkatlolipop@mail.ru>
2025-10-27 19:58:09 +0300

    feat: add Circle

---
+++ src/homework_9/Circle.java
@@ -0,0 +1,19 @@
+package homework_9;
+
+public class Circle extends Shape {
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * this.radius;
+    }
+
+    @Override
+    public double getSquare() {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+}
