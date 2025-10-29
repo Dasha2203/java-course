ef05ed37cb8fdb47f5d25ea1e705d74055260a23
Darya Korbut <kitkatlolipop@mail.ru>
2025-10-29 20:03:00 +0300

    feat: add tostring, hashcode, equals

--- src/homework_10/HomeworkTen.java
+++ src/homework_10/HomeworkTen.java
@@ -1,8 +1,13 @@
 package homework_10;
 
 public class HomeworkTen {
-    static void main(String[] args) {
-        User userOne = new User("Dasha");
-        userOne.hashCode();
+    public static void main(String[] args) {
+        User userOne = new User("Dasha", 15);
+        User userTwo = new User("Dasha", 15);
+
+        System.out.println(userOne.toString());
+        System.out.println(userTwo.toString());
+
+        System.out.println("Пользователи равны? " + userOne.equals(userTwo));
     }
 }
--- src/homework_10/User.java
+++ src/homework_10/User.java
@@ -1,14 +1,33 @@
 package homework_10;
 
+import java.util.Objects;
+
 public class User {
     String name;
+    int age;
 
-    User(String name) {
+    User(String name, int age) {
         this.name = name;
+        this.age = age;
     }
 
     @Override
     public String toString() {
-        return "Name: " + this.name;
+        return "Name: " + this.name + "\n Hash: " + hashCode();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.name, this.age);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+
+        if (obj == null || getClass() != obj.getClass()) return false;
+        User userObj = (User) obj;
+
+        return this.age == userObj.age && Objects.equals(this.name, userObj.name);
     }
 }
