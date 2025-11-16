582ac8ef8f6be37ef631a527ef7a6df3089afaa5
Dasha2203 <kitkatlolipop@mail.ru>
2025-11-16 13:38:53 +0300

    feat: add star task

--- src/homework_13/HomeWorkThirteen.java
+++ src/homework_13/HomeWorkThirteen.java
@@ -2,6 +2,7 @@ package homework_13;
 
 public class HomeWorkThirteen {
     public static void main(String[] args) {
+        // TASK 1
         System.out.println("Valid data: " +
                 Auth.validateCredentials("user123", "password123", "password123"));
 
@@ -13,5 +14,15 @@ public class HomeWorkThirteen {
 
         System.out.println("Length contains space: " +
                 Auth.validateCredentials("user123 d", "password123", "password123"));
+
+        // TASK *: Написать try/catch/finally, в котором finally вызван не будет
+        try {
+            System.out.println("Try");
+            System.exit(0);
+        } catch (Exception e) {
+            System.out.println("block catch");
+        } finally {
+            System.out.println("finally");
+        }
     }
 }
